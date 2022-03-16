c75f3f1ebaf9ed7d5dbb84c94eb24a52a566e847
Camila Modena <itscamilla@hotmail.com>
2022-03-16 18:20:46 +0100

    carousel somewhat works

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["image"]
+
+  connect() {
+    // this.element.textContent = "Hello World!"
+  }
+
+  scrollLeft() {
+
+  }
+
+  scrollRight() {
+
+  }
+}
